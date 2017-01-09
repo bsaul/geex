ebde7a17c410cbc524e10b756bcdf0fc5e6e505f
Bradley Saul <bradleysaul@gmail.com>
2017-01-09 16:44:50 -0500

    creates initial tests for corrections

---
+++ tests/testthat/test_corrections.R
@@ -0,0 +1,44 @@
+context("Finite sample corrections")
+
+set.seed(1)
+n  <- 30
+mu <- 5
+sigma <- 2
+dt <- data.frame(Y = rnorm(n, mean = mu, sd = sigma), id = 1:n)
+
+test_eefun <- function(data){
+  function(theta){
+    with(data,
+         c(Y - theta[1],
+           (Y - theta[1])^2 - theta[2] )
+    )
+  }
+}
+
+bias_estimates <- estimate_equations(eeFUN = test_eefun,
+                                     data  = dt, units = 'id',
+                                     roots = c(1,1),
+                                     corrections = 'bias',
+                                     correction_options = list(b = 0.75))
+
+df1_estimates <- estimate_equations(eeFUN = test_eefun,
+                                data  = dt, units = 'id',
+                                roots = c(1,1),
+                                corrections = 'df1')
+
+df2_estimates <- estimate_equations(eeFUN = test_eefun,
+                                    data  = dt, units = 'id',
+                                    roots = c(1,1),
+                                    corrections = 'df2')
+
+test_that("Bias correction returns matrix", {
+  expect_is(bias_estimates$vcov, 'matrix')
+})
+
+test_that("DF1 correction returns matrix", {
+  expect_is(df1_estimates$vcov, 'matrix')
+})
+
+test_that("DF2 correction returns matrix", {
+  expect_is(df1_estimates$vcov, 'matrix')
+})
