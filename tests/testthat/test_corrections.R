ed72d54a26a57fa32b433fda5176d9bdec10ee94
Bradley Saul <bradleysaul@gmail.com>
2017-01-20 13:33:16 -0500

    update corrections tests

--- tests/testthat/test_corrections.R
+++ tests/testthat/test_corrections.R
@@ -18,18 +18,17 @@ test_eefun <- function(data){
 bias_estimates <- estimate_equations(eeFUN = test_eefun,
                                      data  = dt, units = 'id',
                                      roots = c(1,1),
-                                     corrections = 'bias',
-                                     correction_options = list(b = 0.75))
+                                     corrections_list = list(test = list(fun = fay_bias_correction, options = list(b = 0.75))))
 
 df1_estimates <- estimate_equations(eeFUN = test_eefun,
                                 data  = dt, units = 'id',
                                 roots = c(1,1),
-                                corrections = 'df1')
+                                corrections_list = list(test = list(fun = fay_df_correction, options = list(b = 0.75, L = c(1, 1), version = 1))))
 
 df2_estimates <- estimate_equations(eeFUN = test_eefun,
                                     data  = dt, units = 'id',
                                     roots = c(1,1),
-                                    corrections = 'df2')
+                                    corrections_list = list(test = list(fun = fay_df_correction, options = list(b = 0.75, L = c(1, 1), version = 2))))
 
 test_that("Bias correction returns matrix", {
   expect_is(bias_estimates$vcov, 'matrix')
