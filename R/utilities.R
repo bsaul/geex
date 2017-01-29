3c19205942a48b8852631680214051f5b636ce37
Bradley Saul <bradleysaul@gmail.com>
2017-01-29 11:10:40 -0500

    utility functions for handling model objects

--- R/utilities.R
+++ R/utilities.R
@@ -20,3 +20,46 @@ check_array <- function(object){
     stop('Object is not an array, matrix, or numeric')
   }
 }
+
+#------------------------------------------------------------------------------#
+#' Get the RHS formula from a model object
+#'
+#' @param model a model object such as \code{lm}, \code{glm}, \code{merMod}
+#' @export
+#------------------------------------------------------------------------------#
+get_fixed_formula <- function(model){
+  formula(model, fixed.only = TRUE)[-2]
+}
+
+#------------------------------------------------------------------------------#
+#' Get the LHS formula from a model object
+#'
+#' @param model a model object such as \code{lm}, \code{glm}, \code{merMod}
+#' @export
+#------------------------------------------------------------------------------#
+get_response_formula <- function(model){
+  formula(model)[-3]
+}
+
+#------------------------------------------------------------------------------#
+#' Get a matrix of fixed effects from a model object
+#'
+#' @param rhs_formula the right hand side of a model formula
+#' @param data the data from which to extract the matrix
+#' @export
+#------------------------------------------------------------------------------#
+get_design_matrix <- function(rhs_formula, data){
+  model.matrix(rhs_formula, data)
+}
+
+#------------------------------------------------------------------------------#
+#' Get a vector of responses from a model object
+#'
+#' @param formula model formula
+#' @param data data.frame from which to extract the vector of responses
+#' @export
+#------------------------------------------------------------------------------#
+
+get_response <- function(formula, data){
+  model.response(model.frame(formula, data = data))
+}
