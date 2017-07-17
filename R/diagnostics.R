32f64117bc6319eefcd1f71af4c3d4cea7ee15ef
Bradley Saul <bradleysaul@gmail.com>
2017-07-17 10:53:58 -0400

    renames check_GFUN to verify_roots and updates its documentation

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -1,15 +1,17 @@
 #------------------------------------------------------------------------------#
-#' Check value of estimating eqations
+#' Evaluate estimating eqations.
 #'
-#' Checks that the value of Gm = sum_i psi(O_i, theta) is close to zero when
-#' evaluated at estimated parameters.
+#' Computes the value of \deqn{G_m = sum_i psi(O_i, \hat{\theta})}{G_m = sum_i psi(O_i, theta)}, i.e., the estimating
+#' equations at \code{theta}. Used to verify that \eqn{G_m = 0}{G_m = 0} (or close to 0).
+#'
 #' @param geex_list a list with at least \code{splitdt}, \code{eeFUN}, and \code{outer_eeargs} (which can be \code{NULL})
 #' @param theta parameter estimates to use in evaluating the estimating equations.
+#' @return a numeric vector
 #' @export
 #'
 #------------------------------------------------------------------------------#
 
-check_GFUN <- function(geex_list, theta){
+verify_roots <- function(geex_list, theta){
   psi_i <- create_psi(splitdt      = geex_list$splitdt,
                       eeFUN        = geex_list$eeFUN,
                       outer_eeargs = geex_list$outer_eeargs)
