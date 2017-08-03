956487b0ba96fcc6c051c67a09bb15c721cc9108
Bradley Saul <bradleysaul@gmail.com>
2017-08-03 11:07:17 -0400

    moves compute_matrices to estimate_funs. renames this function to estimate_sandwich_matrices. Also modifies this function to use an m_estimation_basis object. Plus some clean up of documention.

--- R/estimate_funs.R
+++ R/estimate_funs.R
@@ -5,10 +5,11 @@
 #------------------------------------------------------------------------------#
 
 #------------------------------------------------------------------------------#
-#' Compute roots for a set of estimating equations
+#' Estimates roots for a set of estimating equations
 #'
 #' Using the \code{rootFUN} specified by the user (defaults to \code{\link[rootSolve]{multiroot}}),
-#' this function estimates the roots of the equations: \deqn{G_m = sum_i psi(O_i, \hat{\theta}) = 0}{G_m = sum_i psi(O_i, theta) = 0}
+#' this function estimates the roots of the equations:
+#' \deqn{G_m = sum_i psi(O_i, \hat{\theta}) = 0}{G_m = sum_i psi(O_i, theta) = 0}
 #'
 #' This is primilary an internal function used within \code{\link{estimate_equations}},
 #' but it is exported for use in debugging and development.
@@ -49,4 +50,87 @@ estimate_GFUN_roots <- function(basis,
   # Find roots of psi
   rargs <- append(rootFUN_control, list(f = GmFUN))
   do.call(rootFUN, args = rargs)
+}
+
+#------------------------------------------------------------------------------#
+#' Estimate component matrices of the empirical sandwich covariance estimator
+#'
+#' For a given set of estimating equations computes the 'meat' (\eqn{B_m}{B_m}
+#' in Stefanski and Boos notation) and 'bread' (\eqn{A_m}{A_m} in Stefanski and
+#'  Boos notation) matrices necessary to compute the covariance matrix.
+#'
+#' @param basis basis an object of class \code{\linkS4class{m_estimation_basis}}
+#' @param theta vector of parameter estimates (i.e. estimated roots) passed to \code{geex_list$eeFUN}.
+#' @param derivFUN the function used to take derivatives of the estimating equation functions.
+#' Defaults to \code{\link[numDeriv]{jacobian}}.
+#' @param derivFUN_control a list of options passed to \code{\link[numDeriv]{jacobian}}
+#' (or the \code{derivFUN} function).
+#' @inheritParams create_psi
+#'
+#' @return a list with
+#' \itemize{
+#' \item A - the 'bread' matrix
+#' \item B - the 'meat' matrix
+#' \item A_i - a list of 'bread' matrices for each group
+#' \item B_i - a list of 'meat' matrices for each group
+#' }
+#' @details For a set of estimating equations (\eqn{\sum_i \psi(O_i, \theta) = 0}{sum_i \psi(O_i, \theta) = 0}),
+#' this function computes:
+#'
+#' \deqn{A_i =  \partial \psi(O_i, \theta)/\partial \theta}{A_i =  partial \psi(O_i, theta)/\partial \theta}
+#'
+#' \deqn{A =  \sum_i A_i}{A =  \sum_i A_i}
+#'
+#' \deqn{B_i =  \psi(O_i, \theta)\psi(O_i, \theta)^T}{B_i = outer(\psi(O_i, \theta), \psi(O_i, \theta))}
+#'
+#' \deqn{B =  \sum_i B_i}{B =  \sum_i B_i}
+#'
+#' where all of the above are evaluated at \eqn{\hat{\theta}}{hat(\theta)}. The partial derivatives in \eqn{A_i}{A_i}
+#' numerically approximated by the \code{derivFUN}.
+#'
+#' Note that \eqn{A =  \sum_i A_i}{A =  \sum_i A_i} and not \eqn{\sum_i A_i/m}{A =  \sum_i A_i/m}, and the same for \eqn{B}{B}.
+#'
+#' @export
+#' @references Stefanski, L. A., & Boos, D. D. (2002). The calculus of m-estimation. The American Statistician, 56(1), 29-38.
+#------------------------------------------------------------------------------#
+
+estimate_sandwich_matrices <- function(basis,
+                                       theta,
+                                       derivFUN         = numDeriv::jacobian,
+                                       derivFUN_control = list(method = 'Richardson'),
+                                       approxFUN        = NULL,
+                                       approxFUN_control = NULL){
+
+  derivFUN <- match.fun(derivFUN)
+
+  # Create list of estimating eqn functions per unit
+  psi_i <- create_psi(.split_data = basis@.split_data,
+                      .estFUN     = grab_estFUN(basis),
+                      .outer_estFUN_args = basis@.outer_args,
+                      approxFUN = approxFUN,
+                      approxFUN_control = approxFUN_control)
+
+  # Compute the negative of the derivative matrix of estimating eqn functions
+  # (the information matrix)
+  A_i <- lapply(psi_i, function(ee){
+    args <- append(list(fun = ee, x = theta), derivFUN_control)
+    val  <- do.call(derivFUN, args = append(args, basis@.inner_args))
+    -val
+  })
+
+  w <- basis@.weights
+  A_i_pre   <- if(length(w) > 0){ Map(`*`, A_i, w) } else A_i
+  A_i_array <- check_array(simplify2array(A_i_pre))
+  A   <- apply(A_i_array, 1:2, sum)
+
+  # Compute outer product of observed estimating eqns
+  B_i <- lapply(psi_i, function(ee) {
+    ee_val <- do.call(ee, args = append(list(theta = theta), basis@.inner_args))
+    ee_val %*% t(ee_val)
+  })
+  B_i_pre   <- if(length(w) > 0){ Map(`*`, B_i, w) } else B_i
+  B_i_array <- check_array(simplify2array(B_i_pre))
+  B   <- apply(B_i_array, 1:2, sum)
+
+  list(A = A, A_i = A_i, B = B, B_i = B_i)
 }
