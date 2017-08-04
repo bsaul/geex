36cde7f0daa137caceb33946766e00f599092e64
Bradley Saul <bradleysaul@gmail.com>
2017-08-04 15:22:17 -0400

    change how diagnose GFUN works to be based on GFUN rather than a geex list.

--- R/diagnose_funs.R
+++ R/diagnose_funs.R
@@ -9,19 +9,14 @@
 #' Computes the value of \deqn{G_m = sum_i psi(O_i, \hat{\theta})}{G_m = sum_i psi(O_i, theta)}, i.e., the estimating
 #' equations at \code{theta}. Used to verify that \eqn{G_m = 0}{G_m = 0} (or close to 0).
 #'
-#' @param geex_list a list with at least \code{splitdt}, \code{eeFUN}, and \code{outer_eeargs} (which can be \code{NULL})
+#' @param GFUN a function of theta
 #' @param theta parameter estimates to use in evaluating the estimating equations.
 #' @return a numeric vector
 #' @export
 #'
 #------------------------------------------------------------------------------#
 
-diagnose_roots <- function(geex_list, theta){
-  psi_i <- create_psi(splitdt      = geex_list$splitdt,
-                      eeFUN        = geex_list$eeFUN,
-                      outer_eeargs = geex_list$outer_eeargs)
-  GFUN <-  create_GFUN(psi_list = psi_i, inner_eeargs = geex_list$inner_eeargs)
-
+diagnose_roots <- function(GFUN, theta){
   GFUN(theta)
 }
 
