2667cba54ccd8f746bb0b7b6e32810c1d4763f36
Bradley Saul <bradleysaul@gmail.com>
2017-08-04 15:28:04 -0400

    updates test_grab_eeFUN_glm

--- tests/testthat/test_grab_eeFUN_glm.R
+++ tests/testthat/test_grab_eeFUN_glm.R
@@ -8,7 +8,10 @@ test_binomial <- glm(resp ~ age, data = ohio,
                      family = binomial(link = 'logit'))
 
 glm_eefun <- function(data, model){
-  grab_eeFUN(model, data, weights = 2)
+  function(theta){
+    f <- grab_eeFUN(model, data, weights = 2)
+    f(theta)
+  }
 }
 
 test_that("make_eefun returns functions", {
@@ -21,12 +24,12 @@ test_that("make_eefun returns error when length of weight vector is not equal to
 })
 
 test_that("estimate equations obtains correct values for parameters and standard errors ", {
-  x <- estimate_equations(glm_eefun,
-                          data = ohio,
-                          units = 'id',
-                          rootFUN_control  = list(start = c(-1.7, -.11)),
-                          outer_eeargs = list(model = test_binomial))
-  expect_equivalent(x$estimates, coef(test_binomial))
+  x <- m_estimate(glm_eefun,
+                  data = ohio,
+                  units = 'id',
+                  root_control  = new('root_control', .options = list(start = c(-1.7, -.11))),
+                  outer_args = list(model = test_binomial))
+  expect_equivalent(x@estimates, coef(test_binomial))
 
   # Form sandwich estimator "by hand" with the help of sandwich
   psi <- apply(estfun(test_binomial), 2, function(x) tapply(x, test_binomial$data[['id']], sum))
@@ -35,5 +38,5 @@ test_that("estimate equations obtains correct values for parameters and standard
   bbread <- vcov(test_binomial) * n
   Sig <- (bbread %*% mmeat %*% t(bbread))/n
 
-  expect_equal(Sig, x$vcov, tolerance = 1e-5, check.attributes = FALSE)
+  expect_equal(Sig, x@vcov, tolerance = 1e-5, check.attributes = FALSE)
 })
