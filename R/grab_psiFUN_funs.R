db0bd4aadc167613b323f49e7f1b5921efc0e69b
Brian G. Barkley <BarkleyBG@users.noreply.github.com>
2017-09-07 15:10:46 -0400

    handle groups of size 1 in grab_psiFUN.glm
    the `apply()` function will coerce to vector when only one row in the matrix; this should solve that issue for `grab_psiFUN.glm()`

--- R/grab_psiFUN_funs.R
+++ R/grab_psiFUN_funs.R
@@ -96,6 +96,7 @@ grab_psiFUN.glm <- function(object, data, weights = 1, ...){
       V <- phi * diag(1, nrow = n, ncol = n)
     } else if(family_link == 'binomial_logit'){
       D <- apply(X, 2, function(x) x * exp(lp)/((1+exp(lp))^2) )
+      if (n==1) { D <- t(D) } ## apply will undesireably coerce to vector
       V <- phi * diag(f * (1 - f), ncol = length(f) )/length(f)
     }
 
